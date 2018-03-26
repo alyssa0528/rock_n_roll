a4b94bd673e5bf1ce13977ac4110943b9fc5c5cd
Alyssa Kim <alyssa.kim@gmail.com>
2018-03-26 16:06:23 +0000

    fake interface stubbed out

--- lib/rock_n_roll/cli.rb
+++ lib/rock_n_roll/cli.rb
@@ -3,7 +3,8 @@ class RockNRoll::CLI
   def call
     show_list
     menu
-    show_details
+    #show_details
+    exit_program
   end
 
   def show_list #read more on here doc
@@ -15,22 +16,32 @@ class RockNRoll::CLI
   end
 
   def menu
-    puts "Enter the number of the race you'd like more information about:"
-    input = gets.strip
+    input = nil
+    while input != "exit"
+      puts "Enter the number of the race you'd like more information about. Alternatively, type 'list' to see the full list of races, or type exit:"
+      input = gets.strip.downcase
     case input
     when "1"
       puts "race details on race 1"
     when "2"
       puts "race details on race 2"
-    end 
+    when "list"
+      show_list
+    else
+      puts "Sorry, please enter a valid number or type 'list' or 'exit':"
+    end
+    end
   end
 
-  def show_details
-    puts "race date"
-    puts "race distances"
-    puts "race hashtag"
-    puts "race social media links"
-  end
+  #def show_details
+  #  puts "race date"
+  #  puts "race distances"
+  #  puts "race hashtag"
+  #  puts "race social media links"
+  #end
 
+  def exit_program
+    puts "Thanks for checking, and keep training!"
+  end
 
 end
