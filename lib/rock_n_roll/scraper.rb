ef5e79e50033d8c3df55cbed014cd746353c67b2
Alyssa Kim <alyssa.kim@gmail.com>
2018-03-29 15:54:58 +0000

    wrote #scrape_racee_description and hashtag methods, started on #distances method

--- lib/rock_n_roll/scraper.rb
+++ lib/rock_n_roll/scraper.rb
@@ -15,7 +15,11 @@ class RockNRoll::Scraper
 
       @race.location = race_div.css("h5 a").text
       @race.url = race_div.css("h5 a").attribute("href").text
-      scrape_race_site
+      scrape_race_date
+      scrape_race_description
+      scrape_race_hashtag
+      scrape_race_distances
+      #binding.pry
       #binding.pry
     end
     #@race_site = Nokogiri::HTML(open(@race.url))
@@ -24,10 +28,27 @@ class RockNRoll::Scraper
   end
 
   #data is getting incorrectly added to the last @race instance (Luoping) because it was the most recently accessed one...
-  def scrape_race_site #method that parses indiv race site
+  def scrape_race_date #method that parses indiv race site
     @race_site = Nokogiri::HTML(open(@race.url))
     @race_site.search("#hero").each do |header|
     @race.date = header.css("span.subhead").attribute("datetime").value
+    end
+  end
+
+  def scrape_race_description #method that parses indiv race site
+    #binding.pry
+    @race_site = Nokogiri::HTML(open(@race.url))
+    @race_site.search("#features").each do |info_box|
+      #binding.pry
+    @race.description = info_box.css("div.column p").first.text
+    #binding.pry
+    end
+  end
+
+  def scrape_race_hashtag
+    @race_site = Nokogiri::HTML(open(@race.url))
+    @race_site.search("#ribbon").each do |social_media|
+      @race.hashtag = social_media.css("div.hash").text
     end
   end
 
@@ -40,8 +61,16 @@ class RockNRoll::Scraper
 
   def dates
     @race.date
-    binding.pry
   end
+
+  def scrape_race_distances
+    @site = Nokogiri::HTML(open(@race.url"#{the-races/distances/}"))
+    @site.search("div.sidenav").each do |distance|
+      @race.distances = distance.css(".ui-state-default").text
+    end
+  end
+
+end
 
   #def scrape_race_details
     #scrape_races
@@ -56,8 +85,7 @@ class RockNRoll::Scraper
   #      binding.pry
   #      @race.description = info_box.css("div.column.first p").text
 
-  #    @doc.search("#ribbon").each do |social_media|
-  #      @race.hashtag = social_media.css("div.hash").text
+  #
       #  @race.twitter = social_media.css("div.twitter a").attribute("href").text
       #  @race.facebook = social_media.css("div.facebook a").attribute("href").text
       #  @race.instagram = social_media.css("div.instagram a").attribute("href").text
@@ -71,7 +99,3 @@ class RockNRoll::Scraper
       #  binding.pry
       #  @race.distances = distance.css("h3").text
       #end
-
-  def scrape_race_distances
-  end
-end
